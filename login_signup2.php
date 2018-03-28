515e2f18292ca9cd59480138bab450cf24a6e037
AnchalJain <anchalj109@gmail.com>
2018-03-28 22:39:08 +0530

    update the file

--- login_signup2.php
+++ login_signup2.php
@@ -28,63 +28,63 @@
     closeOnSelect: false // Close upon selecting a date,
   });
         });
- 
- 
+
+
  var if_record_exist = false;
- 
+
   $(function(){
     //toggle forms
-    
+
     $('#login').click(function(){
       //get the values
       var email = $('#email').val();
       var password = $('#password').val();
-    
+
       if(!validateEmail(email)){
 				$('.msg').text('Invalid email');
 				return;
 			}
-      
+
       //validate the form
       if(email == '' || password == ''){
         $('.msg').text('Please fill the form');
       }else{
-        $('.msg').html("<div class='preloader-wrapper small active'><div class='spinner-layer spinner-green-only'><div class='circle-clipper left'><div class='circle'></div></div><div class='gap-patch'><div class='circle'></div></div><div class='circle-clipper right'><div class='circle'></div></div></div></div>");     
-        //jQuery ajax post method with 
+        $('.msg').html("<div class='preloader-wrapper small active'><div class='spinner-layer spinner-green-only'><div class='circle-clipper left'><div class='circle'></div></div><div class='gap-patch'><div class='circle'></div></div><div class='circle-clipper right'><div class='circle'></div></div></div></div>");
+        //jQuery ajax post method with
         $.post('php/checklogin.php', {email:email, password:password}, function(resp){
           if(resp == "cool"){
             var x = window.location.search.substr(1);
-            location.href = 'success.php?'+x;
+            location.href = 'index.html';
           }
           else if(resp == "not_activated"){
             $('.msg').html('Please activate your account first and then try to login.');
           }
           else{
-            $('.msg').html('Invalid Email or Password');      
+            $('.msg').html('Invalid Email or Password');
           }
-        }); 
-        
-      } 
+        });
+
+      }
     });
-    
+
     //check if the email is already registered!!
     $('#email-new').blur(function(){
       var email    = $('#email-new').val();
-      
+
       $('.msg').html('');
-      
+
       //checking email
       $.post('php/check-email-registered.php', {email:email}, function(resp){
         if(resp == 'bad'){
-          $('.email_err').text('Email is already registered.');      
+          $('.email_err').text('Email is already registered.');
           if_record_exist = true;
         }else{
-          $('.email_err').text('');     
+          $('.email_err').text('');
           if_record_exist = false;
-        } 
-      }); 
-    }); 
-    
+        }
+      });
+    });
+
     //when click Signup button
     $('#signup').click(function(){
       //get the values
@@ -96,40 +96,41 @@
       var dob= $('.datepicker').val();
       var aadhar= $('#aadhar').val();
       var pan= $('#pan').val();
+      var vechicle= $('#vechicle').val();
       //validate the form
       if(first_name == '' || last_name == '' || email == '' || password == '' || contact == ''){
         $('.msg').text('Please fill the form');
       }else{
         if(if_record_exist == false){
-        $('.msg').html("<div class='preloader-wrapper small active'><div class='spinner-layer spinner-green-only'><div class='circle-clipper left'><div class='circle'></div></div><div class='gap-patch'><div class='circle'></div></div><div class='circle-clipper right'><div class='circle'></div></div></div></div>");     
-        //jQuery ajax post method with 
-        $.post('php/adduser.php', {email:email, password:password, first_name:first_name, last_name:last_name, contact:contact, dob:dob, aadhar:aadhar, pan:pan}, function(resp){
+        $('.msg').html("<div class='preloader-wrapper small active'><div class='spinner-layer spinner-green-only'><div class='circle-clipper left'><div class='circle'></div></div><div class='gap-patch'><div class='circle'></div></div><div class='circle-clipper right'><div class='circle'></div></div></div></div>");
+        //jQuery ajax post method with
+        $.post('php/adduser.php', {email:email, password:password, first_name:first_name, last_name:last_name, contact:contact, vechicle:vechicle, pan:pan}, function(resp){
           if(resp == "done"){
             $(document).ready(function(){
               $('ul.tabs').tabs('select_tab', 'login_form');
             });
-            $('.msg').html('GREAT!! Signed up. Activate your account and login.'); 
+            $('.msg').html('GREAT!! Signed up. Activate your account and login.');
             $('#email-new').val('');
             $('#password-new').val('');
             $('#first_name').val('');
             $('#last_name').val('');
-            $('#contact').val(''); 
+            $('#contact').val('');
            // $('.datepicker').val('');
             //$('#aadhar').val('');
-            $('#pan').val('');       
+            $('#pan').val('');
           }else{
-            $('.msg').html('Something is wrong!' + resp);     
+            $('.msg').html('Something is wrong!' + resp);
           }
-        }); 
+        });
         }else{
-          $('.msg').html('Please fix the above error!');      
-        } 
-      } 
+          $('.msg').html('Please fix the above error!');
+        }
+      }
     });
-    
+
   });
 
-   function validateEmail(email) { 
+   function validateEmail(email) {
 		var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 		return re.test(email);
 	}
@@ -137,7 +138,7 @@
  </script>
 
 
-   
+
 	</head>
 <!-------------------------------------------html body--------------------------------------------------------->
 <body>
@@ -157,7 +158,7 @@
       // The person is not logged into your app or we are unable to tell.
       document.getElementById('status').innerHTML = 'Please log ' +
         'into this app.';
-    
+
     }
   }
 
@@ -173,13 +174,13 @@
   window.fbAsyncInit = function() {
   FB.init({
     appId      : '1439970782705901',
-    cookie     : true,  // enable cookies to allow the server to access 
+    cookie     : true,  // enable cookies to allow the server to access
                         // the session
     xfbml      : true,  // parse social plugins on this page
     version    : 'v2.8' // use graph api version 2.8
   });
 
-  // Now that we've initialized the JavaScript SDK, we call 
+  // Now that we've initialized the JavaScript SDK, we call
   // FB.getLoginStatus().  This function gets the state of the
   // person visiting this page and can return one of three states to
   // the callback you provide.  They can be:
@@ -238,7 +239,7 @@
       <!--Import jQuery before materialize.js-->
       <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
       <script type="text/javascript" src="materialize_css/js/materialize.min.js"></script>
-	  
+
 
 <!-------------------------------------------Translator --------------------------------------------------------->
 
@@ -253,7 +254,7 @@ function googleTranslateElementInit() {
 </script>
 <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
 
-  
+
 <!---------------------------------------------------menu bar-------------------------------------------------->
 	      <div class="navbar-fixed">
     <nav class="white">
@@ -263,18 +264,18 @@ function googleTranslateElementInit() {
         </div>
     </nav>
 </div>
-	
-	
-	
-	
-	
+
+
+
+
+
 	<div class="row">
      <div class="col s12 m8 offset-m2 l6 offset-l3"> <br>
 		  <div class="card-panel grey lighten-5 z-depth-1">
                <div class="row valign-wrapper">
                     <div class="col s12">
 <div class="container center-align">
-       <div class="center-align"> 
+       <div class="center-align">
 	   <ul class="tabs" class = "center-align">
 		   <li class="tab col s3"><a class="active" href="#login_form"><b>Login</b></a></li>
 		   <li class="tab col s3"><a href="#signup_form"><b>Signup</b></a></li>
@@ -285,7 +286,7 @@ function googleTranslateElementInit() {
         <div class="row"><br><br>
         <div class="msg center-align">&nbsp;</div><br>
           <form class="col s12">
-      
+
             <div class="row">
       <h5>You need to login/signup to activate your subscription.</h5>
               <div class="input-field col s12">
@@ -300,7 +301,7 @@ function googleTranslateElementInit() {
                 <label for="password">Password</label>
               </div>
             </div>
-            
+
            <div class="row">
             <div class="col s6">
                    <a href="forgot_password.html" style="color:blue">Forgot password ?</a>
@@ -313,27 +314,27 @@ function googleTranslateElementInit() {
 <fb:login-button scope="public_profile,email" onlogin="checkLoginState();">
 </fb:login-button>
       </div>
-      
+
       <div id="signup_form" class="col s12">
         <div class="row"><br><br>
         <div class="msg center-align">&nbsp;</div><br>
 
           <form class="col s12" >
-            
+
             <div class="row">
               <div class="input-field col s12">
                 <input id="first_name" type="text" class="validate">
                 <label for="first_name">First Name (required)</label>
               </div>
             </div>
-            
-            <div class="row">  
+
+            <div class="row">
               <div class="input-field col s12">
                 <input id="last_name" type="text" class="validate">
                 <label for="last_name">Last Name (required)</label>
               </div>
             </div>
-      
+
             <div class="row">
               <div class="input-field col s12">
                 <input id="email-new" type="email" class="validate"><span class="email_err red-text"></span>
@@ -347,8 +348,8 @@ function googleTranslateElementInit() {
                 <label for="password">Password (required)</label>
               </div>
             </div>
-       
-             <div class="row">  
+
+             <div class="row">
               <div class="input-field col s12">
                 <input id="contact" type="text" class="validate">
                 <label for="contact">Contact (required)</label>
@@ -357,21 +358,21 @@ function googleTranslateElementInit() {
 
 
 
-        <!--    <div class="row">  
+        <!--    <div class="row">
               <div class="input-field col s12">
                 <input type="text" class="datepicker">
                 <label for="dob">Date-of-Birth (required)</label>
               </div>
             </div>
 
-            <div class="row">  
+            <div class="row">
               <div class="input-field col s12">
                 <input id="aadhar" type="text" class="validate">
                 <label for="aadhar">Aadhar Number (optional)</label>
               </div>
             </div>  -->
 
-            <div class="row">  
+            <div class="row">
               <div class="input-field col s12">
                 <input id="pan" type="text" class="validate">
                 <label for="pan" class="blue-text">PAN Card No. (OPTIONAL)</label>
@@ -382,12 +383,12 @@ function googleTranslateElementInit() {
 
 
 			  <div class="col s12">
-				  <br>By clicking on signup I agree to the <a style="color:blue;" href="disclaimer.html">Terms & Conditions and Disclaimer</a> 
+				  <br>By clicking on signup I agree to the <a style="color:blue;" href="disclaimer.html">Terms & Conditions and Disclaimer</a>
               </div>
 
 			  </div>
 
-          
+
 
  <a class="waves-effect waves-light btn green" id="signup">Signup</a>
 
@@ -395,7 +396,7 @@ function googleTranslateElementInit() {
 
 
 </form>
- 
+
         </div>
       </div>
 
@@ -404,6 +405,6 @@ function googleTranslateElementInit() {
         </div>
     </div>
 </div>
-	  
+
 	  </body>
 </html>
