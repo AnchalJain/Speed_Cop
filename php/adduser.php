81549b8b50c2f49554961a6da43dd14862f6f9ee
AnchalJain <anchalj109@gmail.com>
2018-03-28 22:39:25 +0530

    modify user

--- php/adduser.php
+++ php/adduser.php
@@ -1,35 +1,26 @@
 <?php
-session_start();
 
-//echo "jai mata di";
-	//require('db_con.php');
+	require('db_con.php');
 
-	//$link = mysqli_connect('localhost', 'root', 'forceofnature', 'analysis');
-	//$db = mysqli_select_db($link, 'analysis');
-
-
-	//if(mysqli_connect_errno()){echo "failed";}
-
-	$email 		= $_GET['first_name'];
-	//$password	= mysqli_real_escape_string($link, $_POST['password']);
-    //    $fname 		= mysqli_real_escape_string($link, $_POST['first_name']);
-      //  $lname 		= mysqli_real_escape_string($link, $_POST['last_name']);
-        //$contact 		= mysqli_real_escape_string($link, $_POST['contact']);
-        //$dob 		= mysqli_real_escape_string($link, $_POST['dob']);
-        //$aadhar 		= mysqli_real_escape_string($link, $_POST['aadhar']);
-        //$pan 		= mysqli_real_escape_string($link, $_POST['pan']);
-echo $fname ;
-
+	$email 		= mysqli_real_escape_string($link, $_POST['email']);
+	$password	= mysqli_real_escape_string($link, $_POST['password']);
+        $fname 		= mysqli_real_escape_string($link, $_POST['first_name']);
+        $lname 		= mysqli_real_escape_string($link, $_POST['last_name']);
+        $contact 		= mysqli_real_escape_string($link, $_POST['contact']);
+				$vechicle = mysqli_real_escape_string($link, $_POST['vechicle']);
+        // $dob 		= mysqli_real_escape_string($link, $_POST['dob']);
+        // $aadhar 		= mysqli_real_escape_string($link, $_POST['aadhar']);
+        $pan 		= mysqli_real_escape_string($link, $_POST['pan']);
 
 	//convert password to md5 for security
-	//$password 	= md5($password);
+	// $password 	= md5($password);
         // $hash = md5( rand(0,1000) );
 // 	if(!preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/", $email)){
 // 		echo 'The email you have entered is invalid, please try again.';
 // 	}
 // else
 // {
-/*$sql = " insert into user_info (email,password,firstname,lastname,contact,vechicle,pan) values ('anchalj109@gmail.com','2123','Ancahl', 'jain', 123456789,'cuiipcabcu',287437463);";
+$sql = " insert into user_info (email,password,firstname,lastname,contact,vechicle,pan) values ('$email','$password','$fname', '$lname', '$contact','$vechicle','$pan');";
 	$qry = mysqli_query($link, $sql);
         $to      = $email;
         $subject = 'Signup | Verification';
@@ -50,5 +41,5 @@ https://ajayastromoneyguru.com/php/verify.php?email='.$email.'&hash='.$hash.'';
 		echo "done";
 	}
 
-*/
+
 ?>
